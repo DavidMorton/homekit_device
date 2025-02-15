4ee56ea56e319d89905dc87ddbac38449334a339
Sam <sammcj@users.noreply.github.com>
2025-02-15 11:23:40 +1100

    minor updates

--- accessory.py
+++ accessory.py
@@ -2,18 +2,15 @@
 from __future__ import annotations
 
 import logging
-from typing import Any
+from typing import Any, Final
 
-from homeassistant.components.homekit.accessories import HomeAccessory
+from homeassistant.components.homekit import (
+    KNOWN_DEVICES,
+    HomeKitEntity,
+)
 from homeassistant.components.homekit.const import (
     CATEGORY_KETTLE,
-    CATEGORY_THERMOSTAT,
     CATEGORY_FAN,
-    CATEGORY_LIGHTBULB,
-    CATEGORY_HUMIDIFIER,
-    CATEGORY_AIR_PURIFIER,
-    CATEGORY_GARAGE_DOOR_OPENER,
-    CATEGORY_SECURITY_SYSTEM,
     CHAR_ACTIVE,
     CHAR_CURRENT_TEMPERATURE,
     CHAR_TARGET_TEMPERATURE,
@@ -21,30 +18,24 @@ from homeassistant.components.homekit.const import (
     CHAR_ON,
     CHAR_ROTATION_SPEED,
     CHAR_SWING_MODE,
-    CHAR_BRIGHTNESS,
-    CHAR_HUE,
-    CHAR_SATURATION,
-    CHAR_CURRENT_DOOR_STATE,
-    CHAR_TARGET_DOOR_STATE,
-    CHAR_OBSTRUCTION_DETECTED,
-    CHAR_SECURITY_SYSTEM_CURRENT_STATE,
-    CHAR_SECURITY_SYSTEM_TARGET_STATE,
+    PROP_MAX_VALUE,
+    PROP_MIN_VALUE,
+    PROP_STEP_VALUE,
     SERV_THERMOSTAT,
     SERV_FANV2,
-    SERV_LIGHTBULB,
-    SERV_HUMIDIFIER_DEHUMIDIFIER,
-    SERV_AIR_PURIFIER,
-    SERV_GARAGE_DOOR_OPENER,
-    SERV_SECURITY_SYSTEM,
 )
 from homeassistant.const import (
     ATTR_ENTITY_ID,
     STATE_ON,
     STATE_OFF,
+    SERVICE_TURN_ON,
+    SERVICE_TURN_OFF,
     UnitOfTemperature,
 )
 from homeassistant.core import State, callback
-from homeassistant.helpers.event import async_track_state_change
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType
 
 from .const import (
     DOMAIN,
@@ -57,201 +48,247 @@ from .const import (
     CONF_KEEP_WARM_TIME,
     CONF_SPEED_CONTROL,
     CONF_OSCILLATION,
-    CONF_DIRECTION,
-    CONF_BRIGHTNESS,
-    CONF_COLOR_TEMP,
-    CONF_RGB_CONTROL,
-    CONF_CURRENT_HUMIDITY,
-    CONF_TARGET_HUMIDITY,
-    CONF_WATER_LEVEL,
-    CONF_AIR_QUALITY,
-    CONF_FILTER_LIFE,
-    CONF_PM25,
-    CONF_VOC,
-    CONF_DOOR_POSITION,
-    CONF_OBSTRUCTION,
-    CONF_MOTION,
-    CONF_LIGHT_SWITCH,
-    CONF_ALARM_STATE,
-    CONF_SENSORS,
-    CONF_SIREN,
 )
 
-_LOGGER = logging.getLogger(__name__)
+_LOGGER: Final = logging.getLogger(__name__)
 
-class BaseAccessory(HomeAccessory):
-    """Base class for HomeKit accessories."""
 
-    def __init__(self, driver, name, entry_id, aid, config):
-        """Initialize a base accessory object."""
-        super().__init__(driver, name, aid=aid)
-        self.entry_id = entry_id
-        self.config = config
-        self._setup_services()
+class HomeKitDeviceEntity(HomeKitEntity):
+    """Base class for HomeKit device entities."""
 
-    def _setup_services(self):
-        """Set up services for this accessory."""
-        raise NotImplementedError
+    def __init__(self, entry_id: str, unique_id: str) -> None:
+        """Initialize the entity."""
+        super().__init__(unique_id)
+        self._entry_id = entry_id
+        self._attr_unique_id = unique_id
+        self._attr_should_poll = False
 
-    @callback
-    def run(self):
-        """Handle accessory driver started event."""
-        self._setup_state_listeners()
+    @property
+    def device_info(self) -> dict[str, Any]:
+        """Return device info."""
+        return {
+            "identifiers": {(DOMAIN, self._entry_id)},
+            "name": self.name,
+            "manufacturer": "HomeKit Device Aggregator",
+            "model": self.__class__.__name__,
+            "via_device": (DOMAIN, self._entry_id),
+        }
 
-    def _setup_state_listeners(self):
-        """Set up state listeners."""
-        raise NotImplementedError
 
-class KettleAccessory(BaseAccessory):
-    """Kettle accessory implementation."""
+class KettleAccessory(HomeKitDeviceEntity):
+    """Representation of a HomeKit kettle accessory."""
 
-    def _setup_services(self):
-        """Set up services for kettle."""
-        self.category = CATEGORY_KETTLE
+    @property
+    def category(self) -> int:
+        """Return the HomeKit category."""
+        return CATEGORY_KETTLE
+
+    def get_services(self) -> list:
+        """Define services."""
+        services = []
+
+        # Thermostat service (used for kettle functionality)
         serv_thermostat = self.add_preload_service(SERV_THERMOSTAT)
 
-        # Power characteristic
-        self.char_on = serv_thermostat.configure_char(CHAR_ON, value=False)
+        # Power characteristics
+        self.char_on = serv_thermostat.configure_char(
+            CHAR_ON, value=False, setter_callback=self.set_state
+        )
         self.char_active = serv_thermostat.configure_char(CHAR_ACTIVE, value=False)
 
         # Temperature characteristics
-        if CONF_CURRENT_TEMP in self.config:
+        if self._config.get(CONF_CURRENT_TEMP):
             self.char_current_temp = serv_thermostat.configure_char(
-                CHAR_CURRENT_TEMPERATURE, value=0
+                CHAR_CURRENT_TEMPERATURE,
+                value=0,
+                properties={
+                    PROP_MIN_VALUE: 0,
+                    PROP_MAX_VALUE: 100,
+                    PROP_STEP_VALUE: 0.1,
+                },
             )
 
-        if CONF_TARGET_TEMP in self.config:
+        if self._config.get(CONF_TARGET_TEMP):
             self.char_target_temp = serv_thermostat.configure_char(
-                CHAR_TARGET_TEMPERATURE, value=100
+                CHAR_TARGET_TEMPERATURE,
+                value=100,
+                properties={
+                    PROP_MIN_VALUE: 0,
+                    PROP_MAX_VALUE: 100,
+                    PROP_STEP_VALUE: 1,
+                },
+                setter_callback=self.set_target_temperature,
             )
 
-    def _setup_state_listeners(self):
-        """Set up state listeners for kettle."""
-        if CONF_POWER_SWITCH in self.config:
-            async_track_state_change(
-                self.driver.hass,
-                self.config[CONF_POWER_SWITCH],
-                self._async_update_power_state,
-            )
+        services.append(serv_thermostat)
+        return services
 
-        if CONF_CURRENT_TEMP in self.config:
-            async_track_state_change(
-                self.driver.hass,
-                self.config[CONF_CURRENT_TEMP],
-                self._async_update_current_temp,
-            )
+    async def set_state(self, value: bool) -> None:
+        """Set kettle state."""
+        if not self.available:
+            return
 
-        if CONF_TARGET_TEMP in self.config:
-            async_track_state_change(
-                self.driver.hass,
-                self.config[CONF_TARGET_TEMP],
-                self._async_update_target_temp,
-            )
+        service = SERVICE_TURN_ON if value else SERVICE_TURN_OFF
+        await self.hass.services.async_call(
+            "switch",
+            service,
+            {ATTR_ENTITY_ID: self._config[CONF_POWER_SWITCH]},
+            blocking=True,
+        )
+
+    async def set_target_temperature(self, value: float) -> None:
+        """Set target temperature."""
+        if not self.available or CONF_TARGET_TEMP not in self._config:
+            return
+
+        await self.hass.services.async_call(
+            "number",
+            "set_value",
+            {
+                ATTR_ENTITY_ID: self._config[CONF_TARGET_TEMP],
+                "value": value,
+            },
+            blocking=True,
+        )
 
     @callback
-    def _async_update_power_state(self, entity_id: str, old_state: State, new_state: State):
-        """Handle power state changes."""
-        if not new_state:
-            return
-        is_on = new_state.state == STATE_ON
-        self.char_on.set_value(is_on)
-        self.char_active.set_value(is_on)
+    def async_update_state(self, new_state: State) -> None:
+        """Update entity state."""
+        if new_state.entity_id == self._config[CONF_POWER_SWITCH]:
+            is_on = new_state.state == STATE_ON
+            self.char_on.set_value(is_on)
+            self.char_active.set_value(is_on)
 
-    @callback
-    def _async_update_current_temp(self, entity_id: str, old_state: State, new_state: State):
-        """Handle current temperature changes."""
-        if not new_state:
-            return
-        try:
-            temp = float(new_state.state)
-            self.char_current_temp.set_value(temp)
-        except (ValueError, TypeError):
-            _LOGGER.warning("Invalid temperature value: %s", new_state.state)
+        elif (
+            CONF_CURRENT_TEMP in self._config
+            and new_state.entity_id == self._config[CONF_CURRENT_TEMP]
+        ):
+            try:
+                temp = float(new_state.state)
+                self.char_current_temp.set_value(temp)
+            except (ValueError, TypeError):
+                _LOGGER.warning("Invalid temperature value: %s", new_state.state)
 
-    @callback
-    def _async_update_target_temp(self, entity_id: str, old_state: State, new_state: State):
-        """Handle target temperature changes."""
-        if not new_state:
-            return
-        try:
-            temp = float(new_state.state)
-            self.char_target_temp.set_value(temp)
-        except (ValueError, TypeError):
-            _LOGGER.warning("Invalid target temperature value: %s", new_state.state)
+        elif (
+            CONF_TARGET_TEMP in self._config
+            and new_state.entity_id == self._config[CONF_TARGET_TEMP]
+        ):
+            try:
+                temp = float(new_state.state)
+                self.char_target_temp.set_value(temp)
+            except (ValueError, TypeError):
+                _LOGGER.warning("Invalid target temperature value: %s", new_state.state)
 
-class FanAccessory(BaseAccessory):
-    """Fan accessory implementation."""
 
-    def _setup_services(self):
-        """Set up services for fan."""
-        self.category = CATEGORY_FAN
+class FanAccessory(HomeKitDeviceEntity):
+    """Representation of a HomeKit fan accessory."""
+
+    @property
+    def category(self) -> int:
+        """Return the HomeKit category."""
+        return CATEGORY_FAN
+
+    def get_services(self) -> list:
+        """Define services."""
+        services = []
+
+        # Fan service
         serv_fan = self.add_preload_service(SERV_FANV2)
 
         # Basic characteristics
-        self.char_on = serv_fan.configure_char(CHAR_ON, value=False)
+        self.char_on = serv_fan.configure_char(
+            CHAR_ON, value=False, setter_callback=self.set_state
+        )
         self.char_active = serv_fan.configure_char(CHAR_ACTIVE, value=False)
 
         # Optional characteristics
-        if CONF_SPEED_CONTROL in self.config:
-            self.char_speed = serv_fan.configure_char(CHAR_ROTATION_SPEED, value=0)
-
-        if CONF_OSCILLATION in self.config:
-            self.char_swing = serv_fan.configure_char(CHAR_SWING_MODE, value=0)
-
-    def _setup_state_listeners(self):
-        """Set up state listeners for fan."""
-        if CONF_POWER_SWITCH in self.config:
-            async_track_state_change(
-                self.driver.hass,
-                self.config[CONF_POWER_SWITCH],
-                self._async_update_power_state,
+        if self._config.get(CONF_SPEED_CONTROL):
+            self.char_speed = serv_fan.configure_char(
+                CHAR_ROTATION_SPEED,
+                value=0,
+                properties={
+                    PROP_MIN_VALUE: 0,
+                    PROP_MAX_VALUE: 100,
+                    PROP_STEP_VALUE: 1,
+                },
+                setter_callback=self.set_speed,
             )
 
-        if CONF_SPEED_CONTROL in self.config:
-            async_track_state_change(
-                self.driver.hass,
-                self.config[CONF_SPEED_CONTROL],
-                self._async_update_speed,
+        if self._config.get(CONF_OSCILLATION):
+            self.char_swing = serv_fan.configure_char(
+                CHAR_SWING_MODE, value=0, setter_callback=self.set_swing
             )
 
-        if CONF_OSCILLATION in self.config:
-            async_track_state_change(
-                self.driver.hass,
-                self.config[CONF_OSCILLATION],
-                self._async_update_swing,
-            )
+        services.append(serv_fan)
+        return services
+
+    async def set_state(self, value: bool) -> None:
+        """Set fan state."""
+        if not self.available:
+            return
+
+        service = SERVICE_TURN_ON if value else SERVICE_TURN_OFF
+        await self.hass.services.async_call(
+            "switch",
+            service,
+            {ATTR_ENTITY_ID: self._config[CONF_POWER_SWITCH]},
+            blocking=True,
+        )
+
+    async def set_speed(self, value: int) -> None:
+        """Set fan speed."""
+        if not self.available or CONF_SPEED_CONTROL not in self._config:
+            return
+
+        await self.hass.services.async_call(
+            "number",
+            "set_value",
+            {
+                ATTR_ENTITY_ID: self._config[CONF_SPEED_CONTROL],
+                "value": value,
+            },
+            blocking=True,
+        )
+
+    async def set_swing(self, value: int) -> None:
+        """Set swing mode."""
+        if not self.available or CONF_OSCILLATION not in self._config:
+            return
+
+        service = SERVICE_TURN_ON if value else SERVICE_TURN_OFF
+        await self.hass.services.async_call(
+            "switch",
+            service,
+            {ATTR_ENTITY_ID: self._config[CONF_OSCILLATION]},
+            blocking=True,
+        )
 
     @callback
-    def _async_update_power_state(self, entity_id: str, old_state: State, new_state: State):
-        """Handle power state changes."""
-        if not new_state:
-            return
-        is_on = new_state.state == STATE_ON
-        self.char_on.set_value(is_on)
-        self.char_active.set_value(is_on)
+    def async_update_state(self, new_state: State) -> None:
+        """Update entity state."""
+        if new_state.entity_id == self._config[CONF_POWER_SWITCH]:
+            is_on = new_state.state == STATE_ON
+            self.char_on.set_value(is_on)
+            self.char_active.set_value(is_on)
 
-    @callback
-    def _async_update_speed(self, entity_id: str, old_state: State, new_state: State):
-        """Handle speed changes."""
-        if not new_state or not hasattr(self, "char_speed"):
-            return
-        try:
-            speed = float(new_state.state)
-            self.char_speed.set_value(speed)
-        except (ValueError, TypeError):
-            _LOGGER.warning("Invalid speed value: %s", new_state.state)
+        elif (
+            CONF_SPEED_CONTROL in self._config
+            and new_state.entity_id == self._config[CONF_SPEED_CONTROL]
+        ):
+            try:
+                speed = float(new_state.state)
+                self.char_speed.set_value(speed)
+            except (ValueError, TypeError):
+                _LOGGER.warning("Invalid speed value: %s", new_state.state)
 
-    @callback
-    def _async_update_swing(self, entity_id: str, old_state: State, new_state: State):
-        """Handle swing mode changes."""
-        if not new_state or not hasattr(self, "char_swing"):
-            return
-        self.char_swing.set_value(new_state.state == STATE_ON)
+        elif (
+            CONF_OSCILLATION in self._config
+            and new_state.entity_id == self._config[CONF_OSCILLATION]
+        ):
+            self.char_swing.set_value(new_state.state == STATE_ON)
 
-# Add more accessory classes for other device types here...
 
-ACCESSORY_TYPES = {
+ACCESSORY_TYPES: Final[dict[str, type[HomeKitDeviceEntity]]] = {
     "kettle": KettleAccessory,
     "fan": FanAccessory,
-    # Add mappings for other device types as they're implemented
 }
--- homekit.py
+++ homekit.py
@@ -2,22 +2,17 @@
 from __future__ import annotations
 
 import logging
-from typing import Any
+from typing import Any, Final
 
 from homeassistant.components.homekit import (
     DOMAIN as HOMEKIT_DOMAIN,
-    HomeKit,
+    KNOWN_DEVICES,
 )
-from homeassistant.components.homekit.const import (
-    CONF_ENTRY_INDEX,
-)
-from homeassistant.components.homekit.models import HomeKitEntryData
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import (
-    CONF_NAME,
-    EVENT_HOMEASSISTANT_STARTED,
-)
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType
 
 from .const import (
     DOMAIN,
@@ -25,15 +20,15 @@ from .const import (
 )
 from .accessory import ACCESSORY_TYPES
 
-_LOGGER = logging.getLogger(__name__)
+_LOGGER: Final = logging.getLogger(__name__)
 
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
-    async_add_entities: Any,
+    async_add_entities: AddEntitiesCallback,
 ) -> bool:
     """Set up HomeKit integration from a config entry."""
-    _LOGGER.debug("Setting up HomeKit integration for %s", config_entry.data[CONF_NAME])
+    _LOGGER.debug("Setting up HomeKit integration for %s", config_entry.title)
 
     device_type = config_entry.data[CONF_DEVICE_TYPE]
     if device_type not in ACCESSORY_TYPES:
@@ -44,31 +39,43 @@ async def async_setup_entry(
         )
         return False
 
-    # Get the HomeKit entry data
-    entry_data = HomeKitEntryData()
-    entry_data.config_entry = config_entry
-    entry_data.homekit = HomeKit(hass, HOMEKIT_DOMAIN, entry_data, CONF_ENTRY_INDEX)
-
     # Create the appropriate accessory based on device type
     accessory_class = ACCESSORY_TYPES[device_type]
-    accessory = accessory_class(
-        entry_data.homekit.driver,
-        config_entry.data[CONF_NAME],
+    entity = accessory_class(
         config_entry.entry_id,
-        entry_data.aid,
-        config_entry.data,
+        f"{DOMAIN}_{config_entry.entry_id}",
     )
 
-    # Add the accessory to HomeKit
-    entry_data.accessories[(accessory.category, accessory.aid)] = accessory
+    # Store config in entity
+    entity._config = config_entry.data
 
-    # Start HomeKit when Home Assistant is ready
-    async def _async_start_homekit(_):
-        """Start HomeKit."""
-        await entry_data.homekit.async_start()
+    # Add entity to Home Assistant
+    async_add_entities([entity])
 
-    hass.bus.async_listen_once(
-        EVENT_HOMEASSISTANT_STARTED, _async_start_homekit
+    # Register the entity with HomeKit
+    if HOMEKIT_DOMAIN not in hass.data:
+        hass.data[HOMEKIT_DOMAIN] = {}
+    if "pending" not in hass.data[HOMEKIT_DOMAIN]:
+        hass.data[HOMEKIT_DOMAIN]["pending"] = []
+
+    hass.data[HOMEKIT_DOMAIN]["pending"].append(entity)
+
+    # Make sure this device is removed from HomeKit when the config entry is removed
+    config_entry.async_on_unload(
+        lambda: hass.data[HOMEKIT_DOMAIN]["pending"].remove(entity)
+        if entity in hass.data[HOMEKIT_DOMAIN]["pending"]
+        else None
     )
 
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    # This will be handled by Home Assistant's entity registry
+    return True
+
+async def async_remove_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle removal of an entry."""
+    # Remove from KNOWN_DEVICES to allow re-adding
+    unique_id = f"{DOMAIN}_{entry.entry_id}"
+    KNOWN_DEVICES.discard(unique_id)
